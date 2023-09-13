15c62b2e0532776d73712b35452daab2cd7072fe
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-09-13 19:25:42 +0300

    Intro and basic syntax

---
+++ intro-and-basic-syntax/04. Back In 30 Minutes/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int mins = int.Parse(Console.ReadLine());
+
+            mins += 30;
+
+            if(mins > 59)
+            {
+                mins -= 60;
+                hours += 1;
+            }
+
+            if(hours > 23)
+            {
+                hours = 0;
+            }
+
+            Console.WriteLine($"{hours}:{(mins < 10 ? "0" + mins : mins)}");
+        }
+    }
+}
