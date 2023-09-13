2d4433abe554d6a1f14ca2b4234408114adfb042
Kimi Nikolov <126898516+kimin8@users.noreply.github.com>
2023-09-13 20:29:19 +0300

    Intro and basic syntax

---
+++ intro-and-basic-syntax/09. Sum of Odd Numbers/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int c = 0;
+
+            for (int i = 1; c < n; i += 2)
+            {
+                Console.WriteLine(i);
+                sum += i;
+                c++;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
